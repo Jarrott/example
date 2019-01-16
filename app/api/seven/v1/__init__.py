b3df15b61d21d79d33c97eb605d47f1180a57e12
Little student <xqiqio7@gmail.com>
2019-01-16 17:13:13 +0800

    初始化的项目框架

--- app/api/seven/v1/__init__.py
+++ app/api/seven/v1/__init__.py
@@ -2,3 +2,16 @@
 """
 @ Created by Seven on  2019/01/16 <https://www.soo9s.com>
 """
+from flask import Blueprint
+from app.api.seven.v1 import ()
+
+
+def create_blueprint():
+    """
+    版本号为v1
+    将宏图注册到蓝图当中
+    :return: 视图路由
+    """
+    bp_v1 = Blueprint('seven_v1', __name__)
+    # home.api.register(bp_v1)
+    return bp_v1
